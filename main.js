b5bb02c830315d935658f9630e9909a6923a150a
Adi <adij82@gmail.com>
2017-07-30 16:04:32 +0300

    Init repo

---
+++ main.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+hbs.registerHelper('getYear' ,()=> {
+  return new Date().getFullYear()
+})
+
+app.use((req,res, next)  => {
+  const path = req.path;
+  const time = new Date().toString();
+  fs.appendFile('log.txt', time + path + '\n');
+  next();
+});
+
+app.use((req,res,next) => {
+  res.render('maintenance.hbs')
+});
+
+//help from public dir.
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+  res.send({
+    name: 'Adi',
+    age: 34,
+    city: 'Bat-Yam'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    name: 'Adi'
+  })
+  console.info(req.path);
+});
+
+app.get('/welcome', (req, res) => {
+  res.render('welcome.hbs', {
+    title: 'Welcome to my demo!'
+  })
+});
+
+
+app.listen(3000, () => console.info('Magic happens on port 3000'));
